903ca6b9aae075e0ccf634c918ac15148988df14
deivis.ramirez <deivisr25@gmail.com>
2024-06-18 21:10:09 -0500

    Update firebase-config.js
    Eliminar datos de configuración

--- firebase-config.js
+++ firebase-config.js
@@ -11,14 +11,7 @@ import { getStorage } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-st
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBZnRK8vPEfULy4-gA4JB-ZbTRMl0qd74I",
-  authDomain: "apptest-d134c.firebaseapp.com",
-  databaseURL: "https://apptest-d134c-default-rtdb.firebaseio.com",
-  projectId: "apptest-d134c",
-  storageBucket: "apptest-d134c.appspot.com",
-  messagingSenderId: "263377695198",
-  appId: "1:263377695198:web:b9f22d6625c54f295ded4f",
-  measurementId: "G-MLECT3WS8L"
+
 };
 
 // Initialize Firebase
